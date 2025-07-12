2cf49c0b3031c22b7a71394ccb468fd207bd9c69
Faisal <mdfaisalquaiyum004@gmail.com>
2025-07-13 00:14:13 +0530

    added frontend url to server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
-import cors from 'cors';
+import cors from "cors";
 import userRoute from "./Routes/user.js";
 import productRoute from "./Routes/product.js";
 import cartRoute from "./Routes/cart.js";
 import addressRoute from "./Routes/address.js";
-import paymentRouter from './Routes/payment.js'
+import paymentRouter from "./Routes/payment.js";
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-   origin: 'http://localhost:5173', // Replace with your frontend URL
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-})); // Use the CORS middleware with the specified options
+app.use(
+  cors({
+    //  origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: "https://mern-ecommerce-razorpay.vercel.app/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+); // Use the CORS middleware with the specified options
 
 //user route
 app.use("/api/user", userRoute);
@@ -26,7 +29,7 @@ app.use("/api/cart", cartRoute);
 //address route
 app.use("/api/address", addressRoute);
 //payment route
-app.use('/api/payment', paymentRouter)
+app.use("/api/payment", paymentRouter);
 
 mongoose
   .connect(process.env.MONGO_URL, {
